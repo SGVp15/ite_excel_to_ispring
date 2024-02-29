54ce63d3d218e49772b767a4aa2c548badf5d23c
SGVp15 <phosphorusp15@mail.ru>
2024-02-29 14:58:40 +0300

    ticket.py

--- config.py
+++ config.py
@@ -2,8 +2,6 @@ import os
 
 LOG_FILE = os.path.join(os.getcwd(), 'log.txt')
 
-path_questions = os.path.join(os.getcwd(), 'input')
-
 template_file_for_ispring = './template_ispring.xlsx'
 
 input_dir = './input'
