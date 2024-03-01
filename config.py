9fc8acdd5965f85d7c8a08729b6d9cb3fc9172b5
SGVp15 <phosphorusp15@mail.ru>
2024-03-01 12:45:28 +0300

    ticket.py

--- config.py
+++ config.py
@@ -4,5 +4,8 @@ LOG_FILE = os.path.join(os.getcwd(), 'log.txt')
 
 template_file_for_ispring = './template_ispring.xlsx'
 
+# Папка с вопросами
 input_dir = './input'
+
+# output_dir = '//192.168.20.100/личные документы/Савушкин/ispring'
 output_dir = './output'
