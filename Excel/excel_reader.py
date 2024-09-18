b46905de775bde39c554bc58124393835cf3cbf0
SGVp15 <phosphorusp15@mail.ru>
2024-09-18 23:58:53 +0300

    error

--- Excel/excel_reader.py
+++ Excel/excel_reader.py
@@ -54,7 +54,7 @@ def get_all_questions_from_excel_file(file: str) -> [Question]:
             q.id_question = read_excel(wb, page_name, column_id_question, i)
             q.box_question = read_excel(wb, page_name, column_box_question, i)
 
-            image_pattern = r'\s*\[([A-Яа-я\w _]+\.\w+)\]\s*'
+            image_pattern = r'\s*\[([A-Яа-я\w _\-]+\.\w+)\]\s*'
             if re.search(image_pattern, q.text_question):
                 q.image = re.findall(pattern=image_pattern, string=q.text_question)[0]
                 q.image = os.path.join(os.path.dirname(file), q.image.strip())
